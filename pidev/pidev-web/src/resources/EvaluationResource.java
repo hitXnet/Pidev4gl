1271a1b163350499deaf0b62d45b6a6ab34e5b4b
hp <hp@DESKTOP-AE4H3KM>
2019-12-05 06:22:23 +0100

    reunion resource

--- pidev/pidev-web/src/resources/EvaluationResource.java
+++ pidev/pidev-web/src/resources/EvaluationResource.java
@@ -23,7 +23,7 @@ import javax.ws.rs.core.Response.Status;
 import Services.EvaluationSimpleService;
 import Services.userService;
 import Entities.*;
-
+//push
 @Path("Evaluation")
 @RequestScoped
 public class EvaluationResource {
